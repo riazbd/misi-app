43fe5e51f2b18d39f034afc3d46aa314b0fb101b
nuruddin <nuruddinriaz5@gmail.com>
2023-05-16 18:45:31 +0600

    wrote entities in mail template migration file

--- database/migrations/2023_05_16_094831_create_email_templates_table.php
+++ database/migrations/2023_05_16_094831_create_email_templates_table.php
@@ -15,6 +15,9 @@ class CreateEmailTemplatesTable extends Migration
     {
         Schema::create('email_templates', function (Blueprint $table) {
             $table->id();
+            $table->string('mail_type');
+            $table->string('mail_subject');
+            $table->string('mail_body');
             $table->timestamps();
         });
     }
