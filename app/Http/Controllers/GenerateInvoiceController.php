bdde8e8e508c8677c19586da6776d5708981889b
younus <younusmamunu@gmail.com>
2023-09-20 01:09:52 +0600

    invoice added for appointment

--- app/Http/Controllers/GenerateInvoiceController.php
+++ app/Http/Controllers/GenerateInvoiceController.php
@@ -12,16 +12,9 @@ class GenerateInvoiceController extends Controller
 
     public function generatePDF(Request $request, $id)
     {
-        //$appointments = TicketAppointment::all();
-        //dd($id);
-        //$appointment = TicketAppointment::where('id', $id)->first();
 
         $appointment = TicketAppointment::where('id', $id)->first();
-        //$appointment = TicketAppointment::all();
-        //dd($appointment);
-        //$data = ['foo' => 'bar'];; // Your data to pass to the PDF view
         $pdf = PDF::loadView('invoice', compact('appointment'));
-        //dd($pdf);
 
         return $pdf->stream();
     }
